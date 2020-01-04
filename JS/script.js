8e866c4402b0fa62cf2835a17bc5455e874c96fa
Monikarai16 <55135753+Monikarai16@users.noreply.github.com>
2020-01-04 15:31:13 +0530

    Addded initial YWM website files

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+function check_empty() {
+if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+alert("Fill All Fields !");
+} else {
+document.getElementById('form').submit();
+alert("Email sent Successfully...");
+}
+}
